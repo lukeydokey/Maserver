e0e4876be02c225cc72e0e489cd585ebf2f69d74
lukeydokey <pudingles@gmail.com>
2020-10-10 23:02:12 +0900

    maserver

--- index.js
+++ index.js
@@ -2,12 +2,26 @@
 
 var express = require('express');
 var app = express();
+var db_config = require(__dirname + '/config/database.js');
+var conn = db_config.init();
+
+db_config.connect(conn);
 
 app.get('/', function(req, res) {
-  res.send('Hi');
+  var sql = 'SELECT * from Users';
+  conn.query(sql, function(err, rows, fields) {
+    if (err) {
+      console.log('query is not excuted. select fail...\n' + err);
+      throw error;
+    }
+    else{
+      console.log('User info is: ', rows);
+      res.send(rows);
+    }
+  });
 });
 
 var port = 3000;
 app.listen(port, function(){
-  console.log('server on! http://localhost:' + port);
+  console.log('server is running on! http://localhost:' + port);
 });
