50dce972066c268a96f61032e296dca427f096ad
lukeydokey <pudingles@gmail.com>
2020-10-12 20:57:33 +0900

    installed sequelize

---
+++ app.js
@@ -0,0 +1,21 @@
+// app.js
+
+var express = require('express');
+  , app = express();
+  , indexRouter = require('./routes/index');
+  , sequelize = require('./models/index').sequelize;
+const PORT = process.env.PORT || 3000;
+
+//Body parse built-in
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+
+//sync()를 통해 서버를 실행할 때 sequelize가 mysql을 연결 시켜줌
+sequelize.sync();
+
+//라우팅
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+  console.log('Express server is listening on port ' + PORT);
+});
---
+++ models/place.js
@@ -0,0 +1,32 @@
+module.exports =(sequelize, DataTypes) => {
+return sequelize.define('place', {
+  placeid: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+    unique: true,
+  },
+  placename: {
+    type: DataTypes.STRING(45),
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  like: {
+    type: DataTypes.INTEGER.UNSIGNED(10),
+    allowNull: true,
+  },
+  dislike: {
+    type: DataTypes.INTEGER.UNSIGNED(10),
+    allowNull: true,
+  },
+  recomrate: {
+    type: DataTypes.DOUBLE,
+    allowNull: true,
+  }
+}, 
+  {
+    timestamps: false
+  });
+};
